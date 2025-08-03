dc8ebb6817b374115b50d593d4931fac10ad02e9
Prashrijan <shresthaprashrijan@gmail.com>
2025-08-04 00:01:11 +1000

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import {
     Contact,
     Home,
     Login,
+    Orders,
+    PlaceOrders,
     Product,
     SignUp,
 } from "./pages";
@@ -23,7 +25,11 @@ function App() {
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/product/:productId" element={<Product />} />
+
+                {/* private routes */}
                 <Route path="/cart" element={<Cart />} />
+                <Route path="/place-order" element={<PlaceOrders />} />
+                <Route path="/orders" element={<Orders />} />
             </Routes>
         </div>
     );
